517c2e85ed49a430f763138be8469ac90eab92de
dkoval <koval.dmitriy@gmail.com>
2019-09-19 08:25:25 +0200

    Revise public contract of JoinResult

--- flamalgam-project/flamalgam-core/src/main/kotlin/com/github/dkoval/core/dsl/JoinResult.kt
+++ flamalgam-project/flamalgam-core/src/main/kotlin/com/github/dkoval/core/dsl/JoinResult.kt
@@ -6,13 +6,10 @@ interface JoinResult<T : Any> {
     operator fun <U : Any> get(relationship: Relationship.Many<U>): Iterable<U>?
 
     @Throws(NoSuchElementException::class)
-    fun <U : Any> getChild(relationship: Relationship.One<U>): U =
+    fun <U : Any> getOrThrow(relationship: Relationship.One<U>): U =
             this[relationship] ?: throw NoSuchElementException("Result doesn't contain value for $relationship")
 
     @Throws(NoSuchElementException::class)
-    fun <U : Any> getChildren(relationship: Relationship.Many<U>): Iterable<U> =
+    fun <U : Any> getOrThrow(relationship: Relationship.Many<U>): Iterable<U> =
             this[relationship] ?: throw NoSuchElementException("Result doesn't contain value for $relationship")
-
-    @JvmDefault
-    fun <R> map(block: (JoinResult<T>) -> R) = let(block)
 }
