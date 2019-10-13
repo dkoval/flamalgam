5e43353b8128d6775728e90f9395c01699964655
dkoval <koval.dmitriy@gmail.com>
2019-10-13 20:41:59 +0200

    make tests green again

--- flamalgam-project/flamalgam-core/src/main/kotlin/com/github/dkoval/core/dsl/Relationships.kt
+++ flamalgam-project/flamalgam-core/src/main/kotlin/com/github/dkoval/core/dsl/Relationships.kt
@@ -4,12 +4,15 @@ import com.github.dkoval.core.dsl.internal.RelationshipGuard
 import com.github.dkoval.core.event.Event
 import com.github.dkoval.core.event.RekeyedEvent
 import com.github.dkoval.core.event.rekey
+import org.apache.flink.api.common.typeinfo.TypeHint
+import org.apache.flink.api.common.typeinfo.TypeInformation
 import org.apache.flink.api.java.functions.KeySelector
 import org.apache.flink.streaming.api.datastream.DataStream
 import java.util.*
 
-class Relationships<K, V> private constructor(
-        private val parentStream: DataStream<Event<K, V>>) {
+class Relationships<K, V>(
+        private val parentStream: DataStream<Event<K, V>>,
+        private val keyClass: Class<K>) {
 
     private val rekeyedChildStreams: MutableList<DataStream<RekeyedEvent<K>>> = LinkedList()
 
@@ -18,7 +21,7 @@ class Relationships<K, V> private constructor(
                       relationship: Relationship.OneToMany<U>): Relationships<K, V> {
 
         val rekeyedChildStream = childStream
-                .keyBy { it.key }
+                .keyBy({ it.key }, TypeInformation.of(keyClass))
                 .flatMap(RelationshipGuard.forOneToMany(parentKeySelector, relationship.name))
                 .name(relationship.name)
                 .uid(relationship.name)
@@ -30,14 +33,16 @@ class Relationships<K, V> private constructor(
     fun join(): DataStream<RekeyedEvent<K>> {
         val rekeyedParentStream = parentStream
                 .map { it.rekey(it.key, true) }
+                .returns(TypeInformation.of(object : TypeHint<RekeyedEvent<K>>() {}))
 
         return rekeyedParentStream
                 .union(*this.rekeyedChildStreams.toTypedArray())
-                .keyBy { it.key }
+                .keyBy({ it.key }, TypeInformation.of(keyClass))
     }
 
     companion object {
         @JvmStatic
-        fun <K, V> parent(parentStream: DataStream<Event<K, V>>): Relationships<K, V> = Relationships(parentStream)
+        inline fun <reified K, V> parent(parentStream: DataStream<Event<K, V>>): Relationships<K, V> =
+                Relationships(parentStream, K::class.java)
     }
 }
